dd2b8c9ef90af1296ce6ca71fb25e9e7d06abc82
ze <zelkatani@gmail.com>
2017-07-30 21:27:57 +0100

    Add multiple clients

--- server.go
+++ server.go
@@ -3,56 +3,114 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
-type Packet struct {
-	Tiles []struct {
-		Index  uint16 `json:"index"`
-		Owner  uint8  `json:"owner"`
-		Troops uint32 `json:"troops"`
-	} `json:"tiles"`
-	Troops []struct {
-		Owner    uint8  `json:"owner"`
-		Quantity uint32 `json:"quantity"`
-	} `json:"troops"`
+type Tile struct {
+	Index  uint16 `json:"index"`
+	Owner  uint8  `json:"owner"`
+	Troops uint32 `json:"troops"`
 }
 
-// TODO multiple clients
-func main() {
-	fmt.Println("starting server")
+type Packet struct {
+	Tiles  []Tile `json:"tiles"`
+	Player struct {
+		Id      uint8  `json:"id"`
+		Color   string `json:"color"`
+		Capital Tile   `json:"capital"`
+	} `json:"player"`
+}
 
+func (pack Packet) IsEmpty() bool {
+	return pack.Tiles == nil
+}
+
+var clients map[int]*Client
+
+type Client struct {
+	Conn    net.Conn
+	Decoder *json.Decoder
+	Pack    Packet
+	Running bool
+	Id      uint8
+}
+
+func (client *Client) Read() {
+	for {
+		var pack Packet
+		err := client.Decoder.Decode(&pack)
+
+		if err != nil {
+			client.Conn.Close()
+			client.Running = false
+			delete(clients, int(client.Id)-1)
+			fmt.Println("Client disconnected with id:", client.Id)
+			return
+		} else {
+			client.Pack = pack
+			client.Pack.Player.Id = client.Id
+		}
+	}
+}
+
+func (client *Client) Write() {
+	for {
+		if !client.Running {
+			return
+		}
+		for _, cl := range clients {
+			if !cl.Pack.IsEmpty() {
+				var data []byte
+				if cl == client {
+					data, _ = json.Marshal(cl.Pack.Player)
+				} else {
+					data, _ = json.Marshal(cl.Pack)
+				}
+				data = append(data, []byte("\n")...)
+				client.Conn.Write(data)
+			}
+		}
+	}
+}
+
+func (client *Client) Listen() {
+	go client.Read()
+	go client.Write()
+}
+
+func NewClient(conn net.Conn) *Client {
+	decode := json.NewDecoder(conn)
+
+	client := &Client{
+		Conn:    conn,
+		Decoder: decode,
+		Running: true,
+	}
+	client.Listen()
+
+	return client
+}
+
+func main() {
+	fmt.Println("Starting server")
+
+	clients = make(map[int]*Client)
 	l, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("error connecting: ", err.Error())
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	defer l.Close()
 
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("error accepting: ", err.Error())
-			os.Exit(1)
+			log.Fatal(err)
 		}
 
-		go handle(conn)
-	}
-}
-
-func handle(conn net.Conn) {
-	for {
-		decoder := json.NewDecoder(conn)
-
-		var pack Packet
-		err := decoder.Decode(&pack)
-
-		if err != nil {
-			conn.Close()
-			return
-		} else {
-			fmt.Println("packet: ", pack)
-		}
+		client := NewClient(conn)
+		clients[len(clients)] = client
+		client.Id = uint8(len(clients))
+		fmt.Println("New client with id:", client.Id)
 	}
 }
